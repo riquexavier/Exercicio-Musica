d0410f55b3df1970f705a00ffffa59571c6cb5e1
Pedrox | Adm! Dev! <pedrooohhenriquex@hotmail.com>
2024-03-24 13:20:51 -0300

    Resolução do Exercício Música

---
+++ Program.cs
@@ -0,0 +1,207 @@
+﻿Console.WriteLine();
+Console.WriteLine("Bem-vindo(a) venha conhecer a música que tenho escutado recentemente");
+Console.WriteLine();
+
+Console.WriteLine("Presione qualquer tecla para mostrá-la");
+Console.WriteLine();
+Console.WriteLine();
+
+Console.ReadKey(true);
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Fim de Semana No Parque - Racionais MC's");
+Console.ResetColor();  
+Console.WriteLine(); 
+
+Console.WriteLine("À toda comunidade pobre da Zona Sul");
+Console.WriteLine();
+
+Console.WriteLine("Chegou fim de semana todos querem diversão");
+Console.WriteLine("Só alegria nós estamos no verão, mês de janeiro");
+Console.WriteLine("São Paulo, zona sul");
+Console.WriteLine("Todo mundo à vontade, calor céu azul");
+Console.WriteLine("Eu quero aproveitar o Sol");
+Console.WriteLine("Encontrar os camaradas prum basquetebol");
+Console.WriteLine("Não pega nada");
+Console.WriteLine("Estou a uma hora da minha quebrada");
+Console.WriteLine("Logo mais, quero ver todos em paz");
+
+Console.WriteLine();
+
+Console.WriteLine("Um, dois, três carros na calçada");
+Console.WriteLine("Feliz e agitada toda prayboyzada");
+Console.WriteLine("As garagens abertas eles lavam os carros");
+Console.WriteLine("Desperdiçam a água, eles fazem a festa");
+Console.WriteLine("Vários estilos vagabundas, motocicletas");
+Console.WriteLine("Coroa rico boca aberta, isca predileta");
+
+Console.WriteLine();
+
+Console.WriteLine("De verde fluorescente queimada sorridente");
+Console.WriteLine("A mesma vaca loura circulando como sempre");
+Console.WriteLine("Roda a banca dos playboys do Guarujá");
+Console.WriteLine("Muitos manos se esquecem na minha não cresce");
+Console.WriteLine("Sou assim e tô legal, até me leve a mal");
+Console.WriteLine("Malicioso e realista sou eu Mano Brown");
+
+Console.WriteLine();
+
+Console.WriteLine("Me dê 4 bons motivos pra não ser");
+Console.WriteLine("Olha o meu povo nas favelas e vai perceber");
+Console.WriteLine("Daqui eu vejo uma caranga do ano");
+Console.WriteLine("Toda equipada e um tiozinho guiando");
+
+Console.WriteLine();
+
+Console.WriteLine("Com seus filhos ao lado estão indo ao parque");
+Console.WriteLine("Eufóricos brinquedos eletrônicos");
+Console.WriteLine("Automaticamente eu imagino");
+Console.WriteLine("A molecada lá da área como é que tá");
+Console.WriteLine("Provavelmente correndo pra lá e pra cá");
+Console.WriteLine("Jogando bola descalços nas ruas de terra");
+Console.WriteLine("É, brincam do jeito que dá");
+
+Console.WriteLine();
+
+Console.WriteLine("Gritando palavrão é o jeito deles");
+Console.WriteLine("Eles não têm videogame e às vezes nem televisão");
+Console.WriteLine("Mas todos eles têm um dom São Cosme e São Damião");
+Console.WriteLine("A única proteção");
+
+Console.WriteLine();
+
+Console.WriteLine("No último Natal, Papai Noel escondeu um brinquedo");
+Console.WriteLine("Prateado, brilhava no meio do mato");
+Console.WriteLine("Um menininho de 10 anos achou o presente");
+Console.WriteLine("Era de ferro com 12 balas no pente");
+Console.WriteLine("O fim de ano foi melhor pra muita gente");
+
+Console.WriteLine();
+
+Console.WriteLine("Eles também gostariam de ter bicicletas");
+Console.WriteLine("De ver seu pai fazendo cooper tipo atleta");
+Console.WriteLine("Gostam de ir ao parque e se divertir");
+Console.WriteLine("E que alguém os ensinasse a dirigir");
+Console.WriteLine("Mas eles só querem paz e mesmo assim é um sonho");
+Console.WriteLine("Fim de semana no Parque Santo Antônio");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Vamos passear no parque");
+Console.WriteLine("Deixa o menino brincar");
+Console.WriteLine("Fim de semana no parque");
+Console.WriteLine("Vamos passear no parque");
+Console.WriteLine("Vou rezar pra esse domingo não chover");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Olha só aquele clube que da hora");
+Console.WriteLine("Olha aquela quadra, olha aquele campo, olha");
+Console.WriteLine("Olha quanta gente");
+Console.WriteLine("Tem sorveteria, cinema, piscina quente");
+Console.WriteLine("Olha quanto boy, olha quanta mina");
+Console.WriteLine("Afoga essa vaca dentro da piscina");
+Console.WriteLine("Tem corrida de kart dá pra ver");
+Console.WriteLine("É igualzinho o que eu vi ontem na TV");
+
+Console.WriteLine();
+
+Console.WriteLine("Olha só aquele clube que da hora");
+Console.WriteLine("Olha o pretinho vendo tudo do lado de fora");
+Console.WriteLine("Nem se lembra do dinheiro que tem que levar");
+Console.WriteLine("Do seu pai bem louco gritando dentro do bar");
+Console.WriteLine("Nem se lembra de ontem, de hoje e o futuro");
+Console.WriteLine("Ele apenas sonha através do muro");
+
+Console.WriteLine();
+
+Console.WriteLine("Milhares de casas amontoadas");
+Console.WriteLine("Ruas de terra esse é o morro, a minha área me espera");
+Console.WriteLine("Gritaria na feira (vamos chegando)");
+Console.WriteLine("Pode crer eu gosto disso mais calor humano");
+
+Console.WriteLine();
+
+Console.WriteLine("Na periferia a alegria é igual");
+Console.WriteLine("É quase meio dia a euforia é geral");
+Console.WriteLine("É lá que moram meus irmãos, meus amigos");
+Console.WriteLine("E a maioria por aqui se parece comigo");
+Console.WriteLine("E eu também sou o bam bam bam e o que manda");
+Console.WriteLine("O pessoal desde às 10 da manhã está no samba");
+Console.WriteLine("Preste atenção no repique e atenção no acorde");
+Console.WriteLine("(Como é que é Mano Brown?)");
+Console.WriteLine("Pode crer pela ordem");
+
+Console.WriteLine();
+
+Console.WriteLine("A número, número 1 em baixa renda da cidade");
+Console.WriteLine("Comunidade zona sul é, dignidade");
+Console.WriteLine("Tem um corpo no escadão, a tiazinha desce o morro");
+Console.WriteLine("Polícia, a morte, polícia, socorro");
+Console.WriteLine("Aqui não vejo nenhum clube poliesportivo");
+Console.WriteLine("Pra molecada frequentar, nenhum incentivo");
+Console.WriteLine("O investimento no lazer é muito escasso");
+Console.WriteLine("O centro comunitário é um fracasso");
+Console.WriteLine("Mas aí, se quiser se destruir está no lugar certo");
+Console.WriteLine("Tem bebida e cocaína sempre por perto");
+Console.WriteLine("A cada esquina 100, 200 metros");
+Console.WriteLine("Nem sempre é bom ser esperto");
+
+Console.WriteLine();
+
+Console.WriteLine("Schmidt, Taurus, Rossi, Dreher ou Campari");
+Console.WriteLine("Pronúncia agradável, estrago inevitável");
+Console.WriteLine("Nomes estrangeiros que estão no nosso meio pra matar");
+Console.WriteLine("MERDA");
+Console.WriteLine("Como se fosse ontem ainda me lembro");
+Console.WriteLine("7 horas sábado");
+Console.WriteLine("4 de dezembro");
+Console.WriteLine("Uma bala uma moto com 2 imbecis");
+Console.WriteLine("Mataram nosso mano que fazia o morro mais feliz");
+Console.WriteLine("E indiretamente ainda faz, mano Rogério esteja em paz");
+Console.WriteLine("Vigiando lá de cima");
+Console.WriteLine("A molecada do Parque Regina");
+
+Console.WriteLine();
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Vamos passear no parque");
+Console.WriteLine("Deixa o menino brincar");
+Console.WriteLine("Fim de semana no parque");
+Console.WriteLine("Vamos passear no parque");
+Console.WriteLine("Vou rezar pra esse domingo não chover");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Tô cansado dessa porra");
+Console.WriteLine("De toda essa bobagem");
+Console.WriteLine("Alcoolismo, vingança, treta, malandragem");
+Console.WriteLine("Mãe angustiada, filho problemático");
+Console.WriteLine("Famílias destruídas, fins de semana trágicos");
+Console.WriteLine("O sistema quer isso, a molecada tem que aprender");
+Console.WriteLine("Fim de semana no Parque Ipê");
+
+Console.WriteLine();
+
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine("Vamos passear no parque");
+Console.WriteLine("Deixa o menino brincar");
+Console.WriteLine("Fim de semana no parque");
+Console.WriteLine("Vamos passear no parque");
+Console.WriteLine("Vou rezar pra esse domingo não chover");
+Console.ResetColor();
+Console.WriteLine();
+
+Console.WriteLine("Pode crer Racionais MC's e Negritude Júnior juntos");
+Console.WriteLine("Vamos investir em nós mesmos, mantendo distância das");
+Console.WriteLine("Drogas e do álcool");
+
+Console.WriteLine();
+
+Console.WriteLine("Aí rapaziada do Parque Ipê, Jardim São Luiz, Jardim Ingá");
+Console.WriteLine("Parque Arariba, Vaz de Lima");
+Console.WriteLine("Morro do Piolho, Vale das Virtudes e Pirajussara");
+Console.WriteLine();
+
+Console.WriteLine("É isso aí Mano Brown (é isso ai Netinho, paz a todos)");
+Console.WriteLine();
